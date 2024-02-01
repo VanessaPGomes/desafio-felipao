e5e405774e787180a804b9df5ce330179f6652c5
VanessaPGomes <vanessapereira.gomes@hotmal.com>
2024-02-01 14:03:09 -0300

    versão final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Ola")
+let nomeHeroi = 'Naruto';
+let pontos = 20000;
+
+if (pontos <=1000){
+  pontos= ('Ferro')
+}
+  else if (pontos ===1001 || pontos<=2000 ){
+     pontos =('Bronze')
+  }
+  else if (pontos ===2001 || pontos<=5000 ){
+     pontos= ('Prata')
+  }
+  else if (pontos ===5001 || pontos<=7000 ){
+     pontos= ('Ouro')
+  }
+  else if (pontos ===7001 || pontos<=8000 ){
+     pontos= ('Platina')
+  }
+  else if (pontos ===8001 || pontos<=9000 ){
+     pontos= ('Ascedente')
+  }
+  else if (pontos >=10001 ){
+     pontos= ('Radiante')
+  }
+          console.log("O Herói " + nomeHeroi + " está no nível " + pontos)
